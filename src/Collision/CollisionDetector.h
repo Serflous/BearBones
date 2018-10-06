bbe2cc28f4571de10c68418ecd427b818a641048
MaseBruh <masontolman10@gmail.com>
2018-10-06 21:21:11 +0800

    Added TestPointOfIntersection

--- src/Collision/CollisionDetector.h
+++ src/Collision/CollisionDetector.h
@@ -10,13 +10,14 @@ namespace Collision
 	 * An Oriented Bounding Box.
 	 * @author Mathew Causby
 	 * @version 0.1
-	 * @TODO Make Sure in TestOBBOBB that t = glm::vec3(glm::dot(t, aAxis[0]), glm::dot(t, aAxis[2]), glm::dot(t, aAxis[2])) is meant to be x, z, z axes
+	 * @todo Make Sure in TestOBBOBB that t = glm::vec3(glm::dot(t, aAxis[0]), glm::dot(t, aAxis[2]), glm::dot(t, aAxis[2])) is meant to be x, z, z axes
 	 */
 	class CollisionDetector
 	{
 	public:
 		CollisionDetector();
 		bool TestOBBOBB(OBB &a, OBB&b);
+		void TestPointOfIntersection();
 	private:
 
 	};
