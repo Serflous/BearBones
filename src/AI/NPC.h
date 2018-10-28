abf6a42f8292c2141f036ef26f81613f36cea68a
MaseBruh <masontolman10@gmail.com>
2018-10-28 20:52:29 +0800

    Add More Conditions

--- src/AI/NPC.h
+++ src/AI/NPC.h
@@ -1,8 +1,43 @@
 #pragma once
-class NPC
+#include "StateMachine.h"
+#include <iostream>
+
+struct NPCData : public EventData
+{
+	float AngerCo;
+	float HappyCo;
+	float SadCo;
+	float AnnoyedCo;
+};
+
+class NPC : public StateMachine
 {
 public:
-	NPC();
-	~NPC();
+	NPC() : StateMachine(ST_MAX_STATES) {}
+
+	void Walking();
+	void Halt();
+private:
+
+	void ST_Idle(EventData*);
+	void ST_Stop(EventData*);
+	void ST_Start(NPCData*);
+	void ST_Collision(NPCData*);
+
+	// state map to define state function order
+	BEGIN_STATE_MAP
+		STATE_MAP_ENTRY(&NPC::ST_Idle)
+		STATE_MAP_ENTRY(&NPC::ST_Stop)
+		STATE_MAP_ENTRY(&NPC::ST_Start)
+		STATE_MAP_ENTRY(&NPC::ST_Collision)
+	END_STATE_MAP
+
+	enum E_States {
+		ST_IDLE = 0,
+		ST_STOP,
+		ST_START,
+		ST_COLLISION,
+		ST_MAX_STATES
+	};
 };
 
